90f2d8839b1a2de9b5549c642ebf824e5a753ec6
Sandeep Vallabaneni <vallabanenisandeep@gmail.com>
2023-04-13 17:08:38 +0530

    basic empty pages created - routing added

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 
-export default function Home() {
+export default function Budget() {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
@@ -28,7 +28,7 @@ export default function Home() {
           
             <aside id="default-sidebar" className="z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
               <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-              <ul className="space-y-2 font-medium">
+                  <ul className="space-y-2 font-medium">
                     <li>
                         <Link href="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                           <svg aria-hidden="true" className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
